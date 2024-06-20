1aa743bdc2e62e87f14b00eee6117dc6efc1fb59
ramatullah12 <yuantullah@gmail.com>
2024-06-20 20:20:14 +0700

    pust

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DB_URL'),
             'host' => env('MYSQL_ADDON_HOST', '127.0.0.1'),
             'port' => env('MYSQL_ADDON_PORT', '3306'),
             'database' => env('MYSQL_ADDON_DB', 'laravel'),
